3c6e8aa4ccd52ce2143fc8d8530e6a1a5a31c92c
Miguel <miguelrafaelor@gmail.com>
2025-08-21 22:07:30 -0300

    move games on list

--- src/main/java/com/devsuperior/dslist/config/WebConfig.java
+++ src/main/java/com/devsuperior/dslist/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-        
+
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
